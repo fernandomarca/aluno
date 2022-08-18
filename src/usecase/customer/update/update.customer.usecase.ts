a69a17423b90b79b3db028a8c42a82edb8762020
Fernando Marca Magalhães <fernandomarca@hotmail.com>
2022-08-18 19:54:52 -0300

    unit test customer list

---
+++ src/usecase/customer/update/update.customer.usecase.ts
@@ -0,0 +1,35 @@
+import CustomerRepositoryInterface from "../../../domain/customer/repository/customer-repository.interface";
+import { Address } from "../../../domain/customer/value-object/address";
+import { InputUpdateCustomerDto, OutputUpdateCustomerDto } from "./update.customer.dto";
+
+export default class UpdateCustomerUseCase {
+  private CustomerRepository: CustomerRepositoryInterface;
+  constructor(customerRepository: CustomerRepositoryInterface) {
+    this.CustomerRepository = customerRepository;
+  }
+
+  async execute(input: InputUpdateCustomerDto): Promise<OutputUpdateCustomerDto> {
+    const customer = await this.CustomerRepository.find(input.id);
+
+    customer.changeName(input.name);
+    customer.setAddress(new Address(
+      input.address.street,
+      input.address.number,
+      input.address.zip,
+      input.address.city
+    ));
+
+    await this.CustomerRepository.update(customer);
+
+    return {
+      id: customer.id,
+      name: customer.name,
+      address: {
+        street: customer.address.street,
+        number: customer.address.number,
+        zip: customer.address.zip,
+        city: customer.address.city,
+      }
+    }
+  }
+}
